40579f6c429b8a1222335b81a313c2b0e4448336
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-12-11 16:35:20 -0600

    Bookings table

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,7 @@ export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.error("Cabins");
+    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
