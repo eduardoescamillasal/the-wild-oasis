f027d51c89620353cbf77277105cb3d2fee23443
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-12-22 16:40:29 -0600

    sales chart logic

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -22,7 +22,7 @@ function DashboardLayout() {
   } = useRecentStays();
   const { cabins, isLoading: isLoading3 } = useCabins();
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
-  console.log(bookings);
+
   return (
     <StyledDashboardLayout>
       <Stats
@@ -33,7 +33,7 @@ function DashboardLayout() {
       />
       <div>Today&apos;s activity</div>
       <div>Chart stay durations</div>
-      <SalesChart />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
